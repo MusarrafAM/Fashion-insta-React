708ab7a6290c870a474d6690dbf15b373bae0b91
Musarraf <muhammedmusarraf@gmail.com>
2023-10-09 17:21:34 +0530

    Grid setup in collection

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -5,26 +5,29 @@ const Collections = () => {
     <div className="w-full py-16 px-4 lg:h-screen ">
       <div className="py-10 max-w-[1240px] mx-auto">
         <div className="headings text-center mx-auto">
-          <h1 className="md:text-5xl text-4xl font-bold mb-5">Fashion Collection That You <br/> Will Love</h1>
+          <h1 className="md:text-5xl text-4xl font-bold mb-5">
+            Fashion Collection That You <br /> Will Love
+          </h1>
           <p className="md:text-xl text-lg">
-            Fashion service offers innovative and unique styles that go beyond <br/> conventional fashion trends. 
+            Fashion service offers innovative and unique styles that go beyond{" "}
+            <br /> conventional fashion trends.
           </p>
         </div>
 
-        <div className="container-image-gallery flex flex-row">
+        <div className="container-image-gallery grid grid-cols-3">
           <div className="left grid grid-rows-5 grid-cols-3">
             <div className="row-span-5 ">1</div>
             <div className="col-span-2 row-span-3">2</div>
             <div className="col-span-2 row-span-2">3</div>
           </div>
           <div className="middle grid">
-            <div >4</div>
+            <div>4</div>
           </div>
-          {/* <div className="right grid grid-rows-5 grid-cols-3">
-            <div className="col-span-2 row-span-2">5</div>
-            <div className="col-span-2 row-span-3">6</div>
-            <div className="row-span-5">7</div>
-          </div> */}
+          <div className="right grid grid-rows-5 grid-cols-3">
+            <div className="row-span-5 order-1 ">5</div>
+            <div className="col-span-2 row-span-2 order-1">6</div>
+            <div className="col-span-2 row-span-3">7</div>
+          </div>
         </div>
       </div>
     </div>
